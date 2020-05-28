ec7167ade7857bd45feac5cdd7f2e61b9811b6cf
Eric Cote <EricCote@ReactAcademy.ca>
2020-05-28 01:05:31 -0400

    updated dependencies

--- index.js
+++ index.js
@@ -18,9 +18,17 @@ if (require.main === module) {
         // useful when used with OpenAPI-to-GraphQL to locate your application
         setServersFromRequest: true,
       },
+      // cors:  {
+      //   origin: '*',
+      //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      //   preflightContinue: false,
+      //   optionsSuccessStatus: 204,
+      //   maxAge: 86400,
+      //   credentials: true,
+      // }
     },
   };
-  application.main(config).catch(err => {
+  application.main(config).catch((err) => {
     console.error('Cannot start the application.', err);
     process.exit(1);
   });
